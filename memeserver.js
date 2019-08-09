b6f87ce09fa3be8cc927c8e9588419cacb4bf70e
marcomalabag <46800870+marcomalabag@users.noreply.github.com>
2019-08-09 09:29:25 +0800

    Update memeserver.js

--- memeserver.js
+++ memeserver.js
@@ -13,6 +13,7 @@ const mongoose = require("mongoose")
 
 app.use(cookieParser());
 app.use(express.urlencoded());
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs')
 
 function addUser(accounts){
@@ -25,97 +26,113 @@ function addUser(accounts){
 	})
 }
 
+function redirection(req, res){
+	res.render('login.hbs', {msg:"Account does not exist"},
+	function(err, html){
+		res.send(html)
+	})
+}
+
 function findUser(accounts, filter){
 	mongoose.connect(url, function(err){
 		accounts.findOne(filter, function(err, u){
 			if(err) throw err;
-			console.log(u)
+				if(u == null){
+					return filter;
+				}
 			mongoose.connection.close()
 		})
 	})
 }
 
 app.get('/registration', function(req, res){
-	res.render('Registration.hbs', {msg:"", colors:"white"},
+	res.render('Registration.hbs', {msg:"", colorsU:"white", colorsP:"white", colorsCP:"white", colorsB:"white", colorsE:"white"},
 		function(err, html){
 			res.send(html)
 		})
+		
 })
 
 app.post('/registration', function(req, res){
 	var valid = "True";
-	
-	if(req.body.username == "" || req.body.password == ""){
-		if(req.body.username == "")
-		{
-			res.render('Registration.hbs', {msg:"Please enter a username", colorsU:"red"},
+	if(req.body.button == "Signup"){
+		if(req.body.username == "" || req.body.password == ""){
+			if(req.body.username == "")
+			{
+				res.render('Registration.hbs', {msg:"Please enter a username", colorsU:"red"},
+				function(err, html){
+					res.send(html)
+				})
+			}
+			if(req.body.password == "")
+			{
+				res.render('Registration.hbs', {msg:"Please enter a password", colorsP:"red"},
+				function(err, html){
+					res.send(html)
+				})
+			}
+			valid = "False"
+		}
+		if(req.body.password == "" || req.body.Confirm == ""){
+			if(req.body.password == ""){
+				res.render('Registration.hbs', {msg:"Please enter a password", colorsP:"red"},
+				function(err, html){
+					res.send(html)
+				})
+			}
+			if(req.body.Confirm == ""){
+				res.render('Registration.hbs', {msg:"Please enter a confirm password", colorsCP:"red"},
+				function(err, html){
+					res.send(html)
+				})
+			}
+			valid = "False"
+		}
+		else if(req.body.Confirm != req.body.password){
+			res.render('Registration.hbs', {msg:"Password and confirm password do not match. Please try again", colorsP:"red", colorsCP:"red"},
 			function(err, html){
 				res.send(html)
 			})
+			valid = "False"
 		}
-		if(req.body.password == "")
-		{
-			res.render('Registration.hbs', {msg:"Please enter a password", colorsP:"red"},
+		if(req.body.Birthday == ""){
+			res.render('Registration.hbs', {msg:"Please enter your birthday", colorsB:"red"},
 			function(err, html){
 				res.send(html)
 			})
+			valid = "False"
 		}
-		valid = "False"
-	}
-	if(req.body.password == "" || req.body.Confirm == ""){
-		if(req.body.password == ""){
-			res.render('Registration.hbs', {msg:"Please enter a password", colorsP:"red"},
+		if(req.body.Email == ""){
+			res.render('Registration.hbs', {msg:"Please enter an email", colorsE:"red"},
 			function(err, html){
 				res.send(html)
 			})
+			valid = "False"
 		}
-		if(req.body.Confirm == ""){
-			res.render('Registration.hbs', {msg:"Please enter a confirm password", colorsCP:"red"},
+		else if(req.body.Email.indexOf("@") == -1){
+			res.render('Registration.hbs', {msg:"Please enter a valid email", colorsE:"red"},
 			function(err, html){
 				res.send(html)
 			})
+			valid = "False"
 		}
-		valid = "False"
+		if(valid == "True"){
+			var newUser = new accounts({
+				Username:req.body.username,
+				Password:req.body.password,
+				Birthday:req.body.Birthday,
+				Email:req.body.Email,
+				profilepic:req.body.pic
+			})
+			addUser(newUser)
+			res.redirect('/login')
+		}
 	}
-		
-	else if(req.body.Confirm != req.body.password){
-		res.render('Registration.hbs', {msg:"Password and confirm password do not match. Please try again", colorsP:"red", colorsCP:"red"},
-		function(err, html){
-			res.send(html)
-		})
-		valid = "False"
+	else if(req.body.button == "cancel"){
+		res.redirect('/registration')
 	}
-	
-	if(req.body.Birthday == ""){
-		res.render('Registration.hbs', {msg:"Please enter your birthday", colorsB:"red"},
-		function(err, html){
-			res.send(html)
-		})
-		valid = "False"
-	}
-	if(req.body.Email == ""){
-		res.render('Registration.hbs', {msg:"Please enter an email", colorsE:"red"},
-		function(err, html){
-			res.send(html)
-		})
-		valid = "False"
-	}
-	else if(req.body.Email.indexOf("@") == -1){
-		res.render('Registration.hbs', {msg:"Please enter a valid email", colorsE:"red"},
-		function(err, html){
-			res.send(html)
-		})
-		valid = "False"
-	}
-	if(valid == "True"){
-		var newUser = new accounts({
-			Username:req.body.username,
-			Password:req.body.password,
-			Birthday:req.body.Birthday,
-			Email:String
-		})
-		addUser(newUser)
-		
+	else if(req.body.button == "Home"){
+		res.redirect('/')
 	}
 })
 
@@ -123,36 +140,103 @@ app.get('/login', function(req, res){
 	res.render('login.hbs', {msg:""},
 		function(err, html){
 			res.send(html)
+		})	
+})
+
+app.get('/profile', function(req, res){
+	res.render('profile.hbs', {User:"Clarence", Username:"Free_World"},
+		function(err, html){
+			res.send(html)
+		})
+})
+
+app.post('/home_u', function(req, res){
+	/*
+	if(req.cookies.user == undefined){
+		res.redirect('/')
+	}
+	*/
+	if(req.body.button == "Home"){
+		res.render('home_unlog.hbs', 
+		function(err, html){
+			res.send(html)
+		})
+	}
+	else if(req.body.button == "Login")
+	{
+	   res.redirect('/login')
+	}
+	else if(req.body.button == "Sign Up"){
+		res.redirect('/registration')
+	}		
+})
+
+app.get('/home_u', function(req, res){
+	/*
+	if(req.cookies.user == undefined){
+		res.redirect('/')
+	}
+	*/
+		res.render('home_unlog.hbs', 
+		function(err, html){
+			res.send(html)
+		})
+})
+
+app.get('/home', function(req, res){
+	/*
+	if(req.cookies.user == undefined){
+		res.redirect('/')
+	}
+	*/
+		res.render('home.hbs', {User:"Clarence", Username:"Free_World"}, 
+		function(err, html){
+			res.send(html)
 		})
 })
 
 app.post('/login', function(req, res){
 	//res.send(req.body.username);
-	var user = new accounts({username: req.body.username, password: req.body.password})
-	if(findUser(accounts, {username: req.body.username}) == null){
-		res.render('login.hbs', {msg:"<h1>Account does not exit</h1>"},
-		function(err, html){
-			res.send(html)
-		})
+	if(req.body.button == "Home"){
+		res.redirect('/home_u')
 	}
-	else{
-		if(findUser(accounts, {username: req.body.password}) == user.password){
-			res.render('login.hbs', {msg:"<h1>Invalid password please try again</h1>"},
+	else if(req.body.button == "Login"){
+		var user = new accounts({Username: req.body.username, password: req.body.password, profilepic: req.body.pic})
+		console.log(findUser(accounts, {Username: req.body.username}))
+	    /*if(findUser(accounts, {Username: req.body.username})){
+			res.render('login.hbs', {msg:"Account does not exist"},
 			function(err, html){
 			res.send(html)
 			})
 		}
 		else{
-			res.cookie('user', user)
-			res.redirect('/home')
+			if(findUser(accounts, {password: req.body.password}) == user.password){
+				res.cookie('user', user)
+				res.redirect('/')
+			}
+			else{
+				res.render('login.hbs', {msg:"Invalid password please try again"},
+				function(err, html){
+					res.send(html)
+				})
+			}
 		}
+		*/
 	}
+	else if(req.body.button == "Register"){
+		res.redirect('/registration')
+	}
+	else if(req.body.button == "Cancel"){
+		res.redirect('/')
+	}
+	
+	
 })
 
 
 app.get('/', function(req, res){
 	if(req.cookies.user == undefined){
-		res.redirect('/login')
+		res.redirect('/home_u')
 	}
 	else{
 		res.render('home.hbs', {username:req.cookies.user.username},
@@ -160,7 +244,6 @@ app.get('/', function(req, res){
 			res.send(html)
 		})
 		res.redirect('/home')
-		res.send(req.body.username);
 	}
 })
 
