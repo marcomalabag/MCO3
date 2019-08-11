c52cb41520019076c3dbe5e2ad89e78b81c5ea98
marcomalabag <46800870+marcomalabag@users.noreply.github.com>
2019-08-11 16:25:42 +0800

    Update Accounts.js

--- Accounts.js
+++ Accounts.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose")
 
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/Accounts", {
+	useNewUrlParser: true
+})
 
 var accounts = mongoose.model("accounts", {
 	Username:String,
 	Password:String,
+	profilepic:String,
 	Birthday:Date,
 	Email:String,
-	/*memes:[{
-		Caption:String,
-		Picture:String,
-		fileMemeName:String
-	}]
-	*/
+	loggedIn:false,
 })
+
+
 
 module.exports={
 	accounts
