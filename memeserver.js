3898421067812e6f8872cbc3809f83ed25f1dba6
marcomalabag <46800870+marcomalabag@users.noreply.github.com>
2019-08-12 15:27:16 +0800

    Update memeserver.js

--- memeserver.js
+++ memeserver.js
@@ -32,6 +32,38 @@ function findUser(accounts, filter){
 	})
 }
 
+app.post('/editprofile', function(req, res){
+	if(req.body.button == "ChangeName"){
+		accounts.findOneAndUpdate( {Username:req.cookies.account.Username}, {Username:req.body.name}).then((doc)=>{
+			console.log(doc)
+		}, (err)=>{
+			console.log(err)
+		})
+	}
+	else if(req.body.button == "ChangeEmail"){
+		accounts.findOneAndUpdate( {Email: req.cookies.account.Email}, {Email:req.body.Email}).then((doc)=>{
+			console.log(doc)
+		}, (err)=>{
+			console.log(err)
+		})
+	}
+	else if(req.body.button == "ChangePicture"){
+		accounts.findOneAndUpdate( {profilepic: req.cookies.account.profilepic}, {profilepic:req.body.pic}).then((doc)=>{
+			console.log(doc)
+		}, (err)=>{
+			console.log(err)
+		})
+	}
+	res.redirect('/logout')
+})
+
+app.get('/editprofile', function(req, res){
+	res.render('edit_profile.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, Email: req.cookies.account.Email},
+	function(err, html){
+		res.send(html)
+	})
+})
+
 app.get('/registration', function(req, res){
 	res.render('Registration.hbs', {msg:"", colorsU:"white", colorsP:"white", colorsCP:"white", colorsB:"white", colorsE:"white"},
 		function(err, html){
@@ -155,6 +187,37 @@ app.get('/profile', function(req, res){
 		})
 })
 
+app.post('/uploadmeme', function(req, res){
+	if(req.body.button == "Home"){
+		res.redirect('/home')
+	}
+	else if(req.body.button == "Cancel"){
+		res.redirect('/uploadmeme')
+	}
+	else if(req.body.button == "Upload"){
+		var d = new Date();
+		console.log(req.body.pic)
+		console.log(req.body.meme)
+		var newmemes = new memes({Caption: req.body.meme, Picture: req.body.pic, Uploader: req.cookies.account.Username, UploadDate: d})
+		newmemes.save().then((doc)=>{
+			console.log("Successfully added: ", doc)
+			}, (err) => {
+				console.log(err);
+			})
+	}
+	else if(req.body.button == "LogOut"){
+		res.redirect('/logout')
+	}
+	
+})
+
+app.get('/uploadmeme', function(req, res){
+	res.render('upload.hbs', {msg:""}, 
+		function(err, html){
+			res.send(html)
+		})
+})
+
 app.post('/profile', function(req, res){
 	if(req.body.button == "Home"){
 		res.redirect('/home')
@@ -164,6 +227,9 @@ app.post('/profile', function(req, res){
 	}
 	else if(req.body.button == "EditProfile"){
 		res.redirect('/editprofile')
+	}
+	else if(req.body.button == "Upload"){
+		res.redirect('uploadmeme')
 	}
 })
 
@@ -185,15 +251,13 @@ app.post('/home_u', function(req, res){
 })
 
 app.get('/home_u', function(req, res){
-	/*
-	if(req.cookies.user == undefined){
-		res.redirect('/')
-	}
-	*/
-		res.render('home_unlog.hbs', 
-		function(err, html){
-			res.send(html)
-		})
+	memes.find({}).sort({UploadDate: -1}).then((doc)=>{
+		res.render('home_unlog.hbs', {memes: doc
+		});
+		}, (err) => {
+				console.log(err);
+			})
+	
 })
 
 app.post('/home', function(req, res){
@@ -216,10 +280,9 @@ app.get('/home', function(req, res){
 		res.redirect('/')
 	}
 	else{
-		var i;
-		accounts.find({loggedIn:false}).then((doc)=>{
-			res.render('home.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, picture:req.cookies.account.profilepic,
-			user: doc
+		memes.find({}).sort({UploadDate: -1}).then((doc)=>{
+			res.render('home.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, picture:req.cookies.account.profilepic, 
+			memes: doc
 			});
 			}, (err) => {
 				console.log(err);
@@ -249,7 +312,7 @@ app.post('/login', function(req, res){
 						})
 					}
 					else{
-						var user = new accounts({Username: req.body.username, password: req.body.password, profilepic: doc.profilepic})
+						var user = new accounts({Username: req.body.username, password: req.body.password, profilepic: doc.profilepic, Email: doc.Email, })
 						accounts.findOneAndUpdate( {Username:req.body.username}, {loggedIn:true}).then((doc)=>{
 							
 						}, (err)=>{
