4f4697ea2376ba355c7947fada9c6a7cb427c9be
marcomalabag <46800870+marcomalabag@users.noreply.github.com>
2019-08-21 17:06:12 +0800

    Update memeserver.js

--- memeserver.js
+++ memeserver.js
@@ -6,7 +6,7 @@ const fs = require('fs')
 const {accounts} = require("./Accounts.js")
 const {memes} = require("./memes.js")
 const {LikedMemes} = require("./LikedMemes.js")
-
+const {DownloadedMemes} = require("./DownloadedMemes.js")
 
 app.use(cookieParser());
 app.use(express.urlencoded());
@@ -15,38 +15,96 @@ app.use('/profile', express.static(__dirname + '/public'))
 app.use('/viewMeme', express.static(__dirname + '/public'))
 app.set('view engine', 'hbs')
 
-app.get('/likes/:Caption', function(req, res){
-	
-	memes.findOneAndUpdate(req.params, {$inc: {NumLikes : 1}}).then((doc)=>{
-	}, (err)=>{
-	})
-	res.redirect('/home')
-})
-
-app.get('/download/:Picture', function(req, res){
+app.get('/likes/:_id', function(req, res){
 	
 	if(req.cookies.account == null){
 		res.redirect('/home_u')
 	}
-	memes.findOneAndUpdate({Caption: req.params.Picture}, {$inc: {NumDownloads : 1}}).then((doc)=>{
-	}, (err)=>{
-	})
+	else{
+		LikedMemes.findOne({meme_id:req.params._id, user_id:req.cookies.account._id}).then((Likedoc)=>{
+			if(Likedoc == null){
+				var Likes = new LikedMemes({meme_id:req.params._id, user_id:req.cookies.account._id})
+				Likes.save().then((doc)=>{
+					console.log("Successfully added: ", doc)
+				},(err)=>{
+					console.log(err)
+				})
+				memes.findOneAndUpdate({_id: req.params._id}, {$inc: {NumLikes : 1}}).then((doc)=>{
+				
+				}, (err)=>{
+					console.log(err)
+				})
+				res.redirect('/home')
+			}
+			else{
+				LikedMemes.findOneAndDelete({meme_id:req.params._id, user_id:req.cookies.account._id}).then((doc)=>{
+					
+				}, (err)=>{
+					console.log(err)
+				})
+				memes.findOneAndUpdate({_id: req.params._id}, {$inc: {NumLikes : -1}}).then((doc)=>{
+					
+				}, (err)=>{
+					console.log(err)
+				})
+				res.redirect('/home')	
+			}
+		}, (err)=>{
+			
+		})
+	}
+})
+
+app.get('/download/:_id', function(req, res){
 	
-	memes.findOne({Caption: req.params.Picture}).then((doc)=>{
-		if(doc == null){
-			res.redirect('/error')
-		}
-		else{
-			var filepath = __dirname + '/public/' + doc.Picture
-			var fileName = doc.Picture
-			res.download(filepath, fileName);
-			next();
-		}
-	}, (err)=>{
-		console.log(err)
-	})
+	if(req.cookies.account == null){
+		res.redirect('/home_u')
+	}
+	else{
+		memes.findOne(req.params).then((doc)=>{
+			if(doc == null){
+				res.redirect('/error')
+			}
+			else{
+				var filepath = __dirname + '/public/' + doc.Picture
+				var fileName = doc.Picture
+				res.download(filepath, fileName);
+			}
+		}, (err)=>{
+			console.log(err)
+		})
+		
+		DownloadedMemes.findOne({meme_id:req.params._id, user_id:req.cookies.account._id}).then((Likedoc)=>{
+			if(Likedoc == null){
+				var Downloads = new DownloadedMemes({meme_id:req.params._id, user_id:req.cookies.account._id})
+				Downloads.save().then((doc)=>{
+					console.log("Successfully added: ", doc)
+				},(err)=>{
+					console.log(err)
+				})
+				memes.findOneAndUpdate(req.params, {$inc: {NumDownloads : 1}}).then((doc)=>{
+					
+				}, (err)=>{
+					
+				})
+			}
+			else{
+				DownloadedMemes.findOneAndDelete({meme_id:req.params._id, user_id:req.cookies.account._id}).then((doc)=>{
+					
+				}, (err)=>{
+					console.log(err)
+				})
+				memes.findOneAndUpdate({_id: req.params._id}, {$inc: {NumDownloads : -1}}).then((doc)=>{
+					
+				}, (err)=>{
+					console.log(err)
+				})	
+			}
+		}, (err)=>{
+			
+		})
+	}
 	
-	res.redirect('/home')
 })
 
 app.get('/viewAccounts', function(req, res){
@@ -60,8 +118,8 @@ app.get('/viewAccounts', function(req, res){
 	})
 })
 
-app.get('/delete/:Caption', function(req, res){
-	
+app.get('/delete/:_id', function(req, res){
+	console.log(req.params)
 	if(req.cookies.account == undefined){
 		res.redirect('/home_u')
 	}
@@ -87,15 +145,16 @@ app.get('/delete/:Caption', function(req, res){
 	res.redirect('/profile')
 })
 
-app.get('/viewMeme/:Picture', function(req, res){
+app.get('/viewMeme/:_id', function(req, res){
+	
 	
 	if(req.cookies.account == undefined){
-		memes.findOne({Caption: req.params.Picture}).then((doc)=>{
+		memes.findOne(req.params).then((doc)=>{
 			if(doc == null){
 				res.redirect('/error')
 			}
 			else{
-				res.render('ViewMeme_unlog.hbs', {Uploader: doc.Uploader, Caption: doc.Caption, Picture: doc.Picture, UploaderPic: doc.UploaderPic},
+				res.render('ViewMeme_unlog.hbs', {Uploader: doc.Uploader, Caption: doc.Caption, Picture: doc.Picture, UploaderPic: doc.UploaderPic, UploadDate: doc.UploadDate, _id: doc._id},
 				function(err, html){
 					res.send(html)
 				})
@@ -105,12 +164,12 @@ app.get('/viewMeme/:Picture', function(req, res){
 		})
 	}
 	else{
-		memes.findOne({Caption: req.params.Picture}).then((doc)=>{
+		memes.findOne(req.params).then((doc)=>{
 			if(doc == null){
                res.redirect('/error')
 			}
 			else{
-				res.render('ViewMeme.hbs', {Uploader: doc.Uploader, Caption: doc.Caption, Picture: doc.Picture, UploaderPic: doc.UploaderPic, NumLikes: doc.NumLikes, NumDownloads: doc.NumDownloads},
+				res.render('ViewMeme.hbs', {Uploader: doc.Uploader, Caption: doc.Caption, Picture: doc.Picture, UploaderPic: doc.UploaderPic, NumLikes: doc.NumLikes, NumDownloads: doc.NumDownloads, UploadDate: doc.UploadDate, _id: doc._id, idAccount: doc.idAccount},
 				function(err, html){
 					res.send(html)
 				})
@@ -164,6 +223,39 @@ app.post('/editprofile', function(req, res){
 				})
 			}
 		}, (err) =>{
+			console.log(err)
+		})
+	}
+	else if(req.body.button == "Change Password"){
+		
+		accounts.findOne({Username: req.cookies.account.Username}).then((doc2)=>{
+			if(doc2.Password != req.body.password){
+				res.render('edit_profile.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, Email: req.cookies.account.Email, msg: "Incorrect password<br>Please enter again."},
+				function(err, html){
+					res.send(html)
+				})
+			}
+			else{
+				if(req.body.password == req.body.Confirm){
+					accounts.findOne({Username: req.cookies.account.Username}).then((doc1)=>{
+						accounts.findOneAndUpdate( {Password: doc1.Password}, {Password: req.body.New_Password}).then((doc)=>{
+							console.log(doc)
+						}, (err)=>{
+							console.log(err)
+						})
+					}, (err)=>{
+						console.log(err)
+					})
+				}
+				else{
+					res.render('edit_profile.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, Email: req.cookies.account.Email, msg: "Confirmation password and password do not match.<br>Please enter again."},
+					function(err, html){
+						res.send(html)
+					})
+				}
+				res.redirect('/home')
+			}
+		}, (err)=>{
 			console.log(err)
 		})
 	}
@@ -356,48 +448,38 @@ app.get('/login', function(req, res){
 		})	
 })
 
-app.get('/profile/:Uploader', function(req, res){
-	
+app.get('/profile/:idAccount', function(req, res){
+	console.log(req.params)
 	if(req.cookies.account == undefined){
 		res.redirect('/')
 	}
-	else if(req.cookies.account.Username == req.params.Uploader){
-		accounts.findOne({Username: req.params.Uploader}).then((doc1)=>{
-			if(doc1 == null){
-				res.redirect('/error')
-			}
-			else{
-				memes.find(req.params).sort({UploadDate: -1}).then((doc)=>{
-					res.render('profile.hbs', {User:doc1.Username, username:doc1.Username, picture:doc1.profilepic, description: doc1.description,
-					memes: doc
-				});
-				}, (err) => {
-					console.log(err);
-				})
-			}
-			}, (err) =>{
-				console.log(err);
-			})
-	}
 	else{
-		accounts.findOne({Username: req.params.Uploader}).then((doc1)=>{
+		accounts.findOne({_id: req.params.idAccount}).then((doc1)=>{
 			if(doc1 == null){
 				res.redirect('/error')
 			}
-			else{
+			else if(req.cookies.account.Username == doc1.Username){
 				memes.find(req.params).sort({UploadDate: -1}).then((doc)=>{
-					res.render('NonUserprofile.hbs', {User:doc1.Username, username:doc1.Username, picture:doc1.profilepic, description: doc1.description,
+					res.render('profile.hbs', {User:doc1.Username, username:doc1.Username, picture:doc1.profilepic, description: doc1.description, Email: req.cookies.account.Email, _idAccount:doc1._id,
 					memes: doc
 				});
 				}, (err) => {
 					console.log(err);
 				})
 			}
-			}, (err) =>{
-				console.log(err);
-			})
+			else{
+				memes.find(req.params).sort({UploadDate: -1}).then((doc)=>{
+					res.render('NonUserprofile.hbs', {User:doc1.Username, username:doc1.Username, picture:doc1.profilepic, description: doc1.description, Email:doc1.Email,  _idAccount:doc1._id,
+					memes: doc
+					});
+				}, (err) => {
+					console.log(err);
+				})
+			}
+		}, (err) =>{
+			console.log(err);
+		})
 	}
-	
 })	
 	
 
@@ -405,7 +487,7 @@ app.get('/profile/:Uploader', function(req, res){
 app.get('/profile', function(req, res){
 	
 	memes.find({Uploader: req.cookies.account.Username}).sort({UploadDate: -1}).then((doc)=>{
-		res.render('profile.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, picture:req.cookies.account.profilepic, description: req.cookies.account.description,
+		res.render('profile.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, picture:req.cookies.account.profilepic, description: req.cookies.account.description, Email: req.cookies.account.Email, _idAccount:req.cookies.account._id,
 		memes: doc
 		});
 		}, (err) => {
@@ -421,7 +503,7 @@ app.post('/uploadmeme', function(req, res){
 		res.redirect('/uploadmeme')
 	}
 	else if(req.body.button == "Upload"){
-		console.log(req.body.pic.search(".jpg"))
+		console.log(req.body.pic.indexOf(".jpg"))
 		if(req.body.pic == ""){
 			res.render('upload.hbs', {msg:"Please enter a meme"},
 				function(err, html){
@@ -429,56 +511,45 @@ app.post('/uploadmeme', function(req, res){
 				})
 		}
 		
-		if(req.body.pic.search(".jpg") == -1){
-			res.render('upload.hbs', {msg:"Please enter an image file for the meme"},
-				function(err, html){
-				res.send(html)
-				})
-		}
-		
-		else if(req.body.pic.search(".gif") == -1){
-			res.render('upload.hbs', {msg:"Please enter an image file for the meme"},
-				function(err, html){
-				res.send(html)
-				})
-		}
-		
-		else if(req.body.pic.search(".png") == -1){
-			res.render('upload.hbs', {msg:"Please enter an image file for the meme"},
-				function(err, html){
-				res.send(html)
-				})
-		}
-		
-		if(req.body.meme == ""){
-			res.render('upload.hbs', {msg:"Please enter a caption"},
-				function(err, html){
-				res.send(html)
-				})
-		}
-		
 		else{
-			var d = new Date();
-			memes.findOne({Caption: req.body.meme}).then((doc)=>{
-				if(doc == null){
-					var newmemes = new memes({Caption: req.body.meme, Picture: req.body.pic, Uploader: req.cookies.account.Username, UploaderPic: req.cookies.account.profilepic, UploadDate: d, NumLikes: 0, NumDownloads: 0})
-					newmemes.save().then((doc)=>{
-						console.log("Successfully added: ", doc)
-					}, (err) => {
-						console.log(err);
-						})
-					res.redirect('/home')
-				}
-				else{
-					res.render('upload.hbs', {msg: "Caption was already used<br>. Please enter an original caption"},
+			
+			if(req.body.meme == ""){
+				res.render('upload.hbs', {msg:"Please enter a caption"},
+				function(err, html){
+					res.send(html)
+				})
+			}
+			else{
+				if(req.body.pic.indexOf(".jpeg") == -1 && req.body.pic.indexOf(".jpg") == -1 && req.body.pic.search(".gif") == -1 && req.body.pic.search(".png") == -1){
+					res.render('upload.hbs', {msg:"Please enter an image file for the meme"},
 					function(err, html){
 						res.send(html)
 					})
 				}
-			}, (err) =>{
-				console.log(err);
-			})
-		}			
+				else{
+					var d = new Date();
+					memes.findOne({Caption: req.body.meme}).then((doc)=>{
+						if(doc == null){
+							var newmemes = new memes({Caption: req.body.meme, Picture: req.body.pic, Uploader: req.cookies.account.Username, UploaderPic: req.cookies.account.profilepic, UploadDate: d, NumLikes: 0, NumDownloads: 0, idAccount: req.cookies.account._id})
+							newmemes.save().then((doc)=>{
+								console.log("Successfully added: ", doc)
+							}, (err) => {
+								console.log(err);
+							})
+							res.redirect('/home')
+						}
+						else{
+							res.render('upload.hbs', {msg: "Caption was already used<br>. Please enter an original caption"},
+							function(err, html){
+								res.send(html)
+							})
+						}
+					}, (err) =>{
+						console.log(err);
+					})
+				}
+			}
+		}		
 	}
 	else if(req.body.button == "LogOut"){
 		res.redirect('/logout')
@@ -506,10 +577,35 @@ app.post('/profile', function(req, res){
 		res.redirect('/editprofile')
 	}
 	else if(req.body.button == "Upload"){
-		res.redirect('uploadmeme')
+		res.redirect('/uploadmeme')
 	}
 	else if(req.body.button == "Users"){
-		res.redirect('viewAccounts')
+		res.redirect('/viewAccounts')
+	}
+	else if(req.body.button == "Date"){
+		memes.find({}).sort({UploadDate: -1}).then((doc)=>{
+		res.render('home.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, picture: req.cookies.account.profilepic ,Email:req.cookies.account.Email, description:req.cookies.account.description, memes: doc
+		});
+		}, (err) => {
+				console.log(err);
+			})
+	}
+	else if(req.body.button == "Likes"){
+		memes.find({}).sort({NumLikes: -1}).then((doc)=>{
+		res.render('home.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, picture: req.cookies.account.profilepic ,Email:req.cookies.account.Email, description:req.cookies.account.description, memes: doc
+		});
+		}, (err) => {
+				console.log(err);
+			})
+		
+	}
+	else if(req.body.button == "Downloads"){
+		memes.find({}).sort({NumDownloads: -1}).then((doc)=>{
+		res.render('home.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, picture: req.cookies.account.profilepic ,Email:req.cookies.account.Email, description:req.cookies.account.description, memes: doc
+		});
+		}, (err) => {
+				console.log(err);
+			})
 	}
 	
 })
@@ -548,11 +644,41 @@ app.post('/home', function(req, res){
 	   res.redirect('/logout')
 	}
 	else if(req.body.button == "Profile"){
-		res.redirect('/profile/' + req.cookies.account.Username)
+		res.redirect('/profile/' + req.cookies.account._id)
 	}
-    else{
-		console.log(req.body.button)
-	}		
+    else if(req.body.button == "Date"){
+		memes.find({}).sort({UploadDate: -1}).then((doc)=>{
+		res.render('home.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, picture: req.cookies.account.profilepic ,Email:req.cookies.account.Email, description:req.cookies.account.description, memes: doc
+		});
+		}, (err) => {
+				console.log(err);
+			})
+	}
+	else if(req.body.button == "Likes"){
+		memes.find({}).sort({NumLikes: -1}).then((doc)=>{
+		res.render('home.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, picture: req.cookies.account.profilepic ,Email:req.cookies.account.Email, description:req.cookies.account.description, memes: doc
+		});
+		}, (err) => {
+				console.log(err);
+			})
+		
+	}
+	else if(req.body.button == "Downloads"){
+		memes.find({}).sort({NumDownloads: -1}).then((doc)=>{
+		res.render('home.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, picture: req.cookies.account.profilepic ,Email:req.cookies.account.Email, description:req.cookies.account.description, memes: doc
+		});
+		}, (err) => {
+				console.log(err);
+			})
+	}
+	else if(req.body.button == "MemeOfTheMonth"){
+		memes.find({}).sort({NumDownloads: -1, NumLikes: -1}).then((doc)=>{
+		res.render('home.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, picture: req.cookies.account.profilepic ,Email:req.cookies.account.Email, description:req.cookies.account.description, memes: doc
+		});
+		}, (err) => {
+				console.log(err);
+			})
+	}
 })
 
 app.get('/home', function(req, res){
@@ -562,7 +688,7 @@ app.get('/home', function(req, res){
 	}
 	else{
 		memes.find({}).sort({UploadDate: -1}).then((doc)=>{
-			res.render('home.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, picture: req.cookies.account.profilepic ,Email:req.cookies.account.Email, description:req.cookies.account.description,
+			res.render('home.hbs', {User:req.cookies.account.Username, username:req.cookies.account.Username, picture: req.cookies.account.profilepic ,Email:req.cookies.account.Email, description:req.cookies.account.description, _idAccount:req.cookies.account._id,
 			memes: doc
 			});
 			}, (err) => {
@@ -572,44 +698,60 @@ app.get('/home', function(req, res){
 })
 
 app.post('/login', function(req, res){
-	//res.send(req.body.username);
 	if(req.body.button == "Home"){
 		res.redirect('/home_u')
 	}
 	else if(req.body.button == "Login"){
-		accounts.findOne({Username:req.body.username}).then((doc)=>{
-			if(doc == null){
-				res.render('login.hbs', {msg:"Account does not exist"},
-				function(err, html){
+		
+		if(req.body.username == ""){
+			res.render('login.hbs', {msg:"Please enter an account"},
+			function(err, html){
 				res.send(html)
+			})
+		}
+		
+		else{
+			if(req.body.password == ""){
+				res.render('login.hbs', {msg:"Please enter a password"},
+				function(err, html){
+					res.send(html)
 				})
 			}
 			else{
-				accounts.findOne({Password:req.body.password}).then((doc)=>{
+				accounts.findOne({Username:req.body.username}).then((doc)=>{
 					if(doc == null){
-						res.render('login.hbs', {msg:"Invalid password please try again"},
+						res.render('login.hbs', {msg:"Account does not exist"},
 						function(err, html){
 							res.send(html)
 						})
 					}
 					else{
-						var user = new accounts({Username: req.body.username, password: req.body.password, profilepic: doc.profilepic, Email: doc.Email, description: doc.description})
-						accounts.findOneAndUpdate( {Username:req.body.username}, {loggedIn:true}).then((doc)=>{
-							
+						accounts.findOne({Password:req.body.password}).then((doc)=>{
+							if(doc == null){
+								res.render('login.hbs', {msg:"Invalid password please try again"},
+								function(err, html){
+									res.send(html)
+								})
+							}
+							else{
+								var user = new accounts({Username: req.body.username, password: req.body.password, profilepic: doc.profilepic, Email: doc.Email, description: doc.description, _id:doc._id})
+								accounts.findOneAndUpdate( {Username:req.body.username}, {loggedIn:true}).then((doc)=>{
+									
+								}, (err)=>{
+									console.log(err)
+								})
+								res.cookie('account', user)
+								res.redirect('/')
+							}
 						}, (err)=>{
 							console.log(err)
 						})
-						res.cookie('account', user)
-						res.redirect('/')
 					}
 				}, (err)=>{
 					console.log(err)
 				})
 			}
-		}, (err)=>{
-			console.log(err)
-		accounts.closeDatabase();	
-		})
+		}
 	}
 	else if(req.body.button == "Register"){
 		res.redirect('/registration')
